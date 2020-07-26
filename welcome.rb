dff2910d70a4ecdd275d730bb8921aadd66252ca
Ross Buddie <66185926+rossme@users.noreply.github.com>
2020-07-26 14:33:45 +0100

    Delete welcome.rb

--- welcome.rb
+++
@@ -1,9 +0,0 @@
-def welcome
-  puts "**********************************************"
-  puts "***                                        ***"
-  puts "***    WELCOME to ROCK, PAPER, SCISSORS    ***"
-  puts "***               LOADING...               ***"
-  puts "***                                        ***"
-  puts "**********************************************"
-end
-welcome
